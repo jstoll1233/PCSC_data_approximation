6c35e7f5a4b69f9044cb91b04a65222805b1ce47
Stoll Justine Emilie <jstoll@INTRANET.EPFL.CH@MA-LINUX-19.intranet.epfl.ch>
2020-11-28 20:07:02 +0100

    CMakeList, template file, small modifs in DataHandler

--- src/DataHandler.cpp
+++ src/DataHandler.cpp
@@ -4,45 +4,65 @@
 
 #include "DataHandler.h"
 #include <sstream>
-DataHandler::DataHandler(std::string file_name, int dimension, int number_data_points)
-: dim(dimension), num_dp(number_data_points)
+DataHandler::DataHandler(std::string file_name)
 {
     std::ifstream file(file_name);
-    if (not file.is_open())
+    if (not file.is_open()) {
         throw std::runtime_error("file not found ");
-    else
-        // Allocation of the memory for the data matrix
-        data = new double* [num_dp];
-        for (int i = 0; i < num_dp; ++ i){
-            data[i] = new double [dim];
-        }
-
-
+    }
+    else{
         read_data(std::ifstream file);
+    }
 }
 
 void DataHandler::read_data(std::ifstream file) {
-    std::string line;
-    std::getline(file, line);
-    //here we should check that dimension and line.size() are the same.
+
+    std::getline(file, line, ',');
+    if (line == "\uFEFFdimension"){
+        std::getline(file, line);
+        dim = std::stod(line);
+        var = new std::string [dim];
+    }
+
+    std::getline(file, line, ',');
+    if (line == "number_of_points"){
+        std::getline(file, line);
+        num_dp = std::stod(line);
+    }
+
+    std::getline(file, line, ',');
+    if (line == "variables"){
+        std::getline(file, line);
+        std::stringstream  ss(line);
+
+        int counter =0;
+        while (std::getline(ss, line, ',')){
+            var[counter] = line;
+            ++counter;
+        }
+    }
+    //Allocation
+    data = new double* [num_dp];
+    for (int i = 0; i < num_dp; ++ i){
+        data[i] = new double [dim];
+    }
+
     int l = 0;
     int c = 0;
-    while (std::getline(file, line)){
+    while(std::getline(file, line)){
         c = 0;
         std::stringstream ss(line);
-        while (std::getline(ss, line, ',')){
-            data[l][c] = std::stod(line);
+        while(getline(ss, line, ',')){
+            A[l][c] = std::stod(line);
             ++c;
         }
         ++l;
-
     }
-
 }
+
 
 double ** DataHandler::get_data() {
     return data;
-
 }
 
 DataHandler::~DataHandler() {
@@ -51,4 +71,5 @@ DataHandler::~DataHandler() {
     }
     delete [] data;
 
+    delete [] var;
 }
