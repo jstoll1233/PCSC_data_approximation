f4c184feb002eab80136bc44fff168a2af5a4f01
Kseniia Shevchenko <kseniia.shevchenko@epfl.ch>
2020-12-04 01:06:43 +0100

    Adding classes for Lagrange and Barycentric interpolation

---
+++ src/Lagrange.h
@@ -0,0 +1,21 @@
+//
+// Created by kshevche@INTRANET.EPFL.CH on 01.12.20.
+//
+
+#ifndef PCSC_EXERCISES_LAGRANGE_H
+#define PCSC_EXERCISES_LAGRANGE_H
+
+#include "Interpolation.h"
+
+/**
+ * Class Lagrange for calculating Lagrange interpolating polynomial
+ */
+class Lagrange: public Interpolation {
+private:
+    double Numerator(const DataHandler& data_, int k, double point);
+public:
+    double Calculate_value_of_interpolant(const DataHandler& data_, const double point) override;
+};
+
+
+#endif //PCSC_EXERCISES_LAGRANGE_H
