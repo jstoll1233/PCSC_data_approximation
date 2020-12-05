1c85ab8c9407a5c1e2184a492ecd2d80c97eeb48
Stoll Justine Emilie <jstoll@INTRANET.EPFL.CH@MA-LINUX-04.intranet.epfl.ch>
2020-12-05 19:28:30 +0100

    usr-program interaction

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,21 @@ int main(int argc, char ** argv){
 
     try {
         DataHandler handler(path);
-        int dim = handler.get_dimension();
-        std::cout << "Dimension " << dim;
+        int dimension = handler.get_dimension();
+        int num_data_points = handler.get_number_data_points();
+        std::cout << "The problem has a dimension of " << dimension <<
+                    " and contains " << num_data_points << " data points." << std::endl;
+
+        int method=0;
+        std::cout << "Which method among the following should be used ? " << std::endl;
+        std::cout << "1. Least squares " << std::endl;
+        std::cout << "2. Lagrange interpolation " << std::endl;
+        std::cout << "3. Barycentric interpolation " << std::endl;
+        while ((method != 1) and (method != 2) and (method != 3)) {
+            std::cout << "Please enter a number from 1 to 3: ";
+            std::cin >> method;
+        }
+
 
     }
 
