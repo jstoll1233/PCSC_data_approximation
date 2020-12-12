90d72f03717338fa8fda7e299b0e485f7ed59eaa
Stoll Justine Emilie <jstoll@INTRANET.EPFL.CH@MA-LINUX-04.intranet.epfl.ch>
2020-12-12 23:15:54 +0100

    fix tests that didnt pass because of changes in classes

--- test/test.cpp
+++ test/test.cpp
@@ -29,17 +29,17 @@ int n_points = DataHandler_t.get_number_data_points();
 double** data =DataHandler_t.get_data();
 
 TEST(datahandler, header_reading){
-    EXPECT_EQ(dimension, 3);
-    EXPECT_EQ(n_points, 5);
+    EXPECT_EQ(dimension, 2);
+    EXPECT_EQ(n_points, 4);
 
 }
 
 TEST(datahandler, data_reading){
-    EXPECT_EQ(data[0][0], 1.2);
-    EXPECT_EQ(data[0][dimension-1], 2.8);
-    EXPECT_EQ(data[2][1], 3.1);
-    EXPECT_EQ(data[n_points-1][0], 6.7);
-    EXPECT_EQ(data[n_points-1][dimension-1], 8.8);
+    EXPECT_EQ(data[0][0], 1.0);
+    EXPECT_EQ(data[0][dimension-1], 3);
+    EXPECT_EQ(data[2][1], 67);
+    EXPECT_EQ(data[n_points-1][0], 4);
+    EXPECT_EQ(data[n_points-1][dimension-1], 165);
 
 }
 
@@ -112,53 +112,43 @@ int dimension_ =  DataHandler_t_.get_dimension();
 int n_points_ =  DataHandler_t_.get_number_data_points();
 double** data_ = DataHandler_t_.get_data();
 
-Lagrange lagrange;
+Lagrange lagrange(DataHandler_t_);
+double nan_ = std::numeric_limits<double>::quiet_NaN();
 
 TEST(lagrange, point_exception){
-    try {
-        lagrange.Calculate_value_of_interpolant(DataHandler_t_, 0.5);
-        FAIL() << "Expected std::range_error";
-    }
-    catch (std::range_error &e) {
-        EXPECT_EQ(e.what(), std::string("The point does not belong to the interval"));
-    }
-    catch (...) {
-        FAIL() << "Expected std::range_error";
-    }
+    nan_ = lagrange.Calculate_value_of_interpolant(0.5);
+    EXPECT_EQ(std::isnan(nan_),1);
 }
 
 
 TEST(lagrange, lagrange_calculation) {
-    EXPECT_NEAR(lagrange.Calculate_value_of_interpolant(DataHandler_t_, 5), 12.80964, 0.001);
+    EXPECT_NEAR(lagrange.Calculate_value_of_interpolant(3), 67, 0.001);
+    EXPECT_NEAR(lagrange.Calculate_value_of_interpolant(2.5), 37.875, 0.001);
 }
 
 
-Barycentric barycentric;
+Barycentric barycentric(DataHandler_t_);
 
 TEST(barycentric, point_exception){
-    try {
-        barycentric.Calculate_value_of_interpolant(DataHandler_t_, 0.5);
-        FAIL() << "Expected std::range_error";
-    }
-    catch (std::range_error &e) {
-        EXPECT_EQ(e.what(), std::string("The point does not belong to the interval"));
-    }
-    catch (...) {
-        FAIL() << "Expected std::range_error";
-    }
+    nan_ = barycentric.Calculate_value_of_interpolant(5);
+    EXPECT_EQ(std::isnan(nan_),1);
 }
 
+
 TEST(barycentric, barycentric_calculation) {
-    EXPECT_NEAR(barycentric.Calculate_value_of_interpolant(DataHandler_t_, 5), 12.80964, 0.001);
+    EXPECT_NEAR(barycentric.Calculate_value_of_interpolant(4), 165, 0.001);
+    EXPECT_NEAR(lagrange.Calculate_value_of_interpolant(2.5), 37.875, 0.001);
+
 }
 
 //Eigen::VectorXf vect(3);
 //vect(0) = -0.0144, vect(1) = 0.192, vect(2) = 3.697;
+LeastSquares leastsquares(DataHandler_t_, 3);
 
 TEST(least_squares, least_squares_calculation) {
-    EXPECT_NEAR(LeastSquares(DataHandler_t_, 2)[0], 3.697, 0.001);
-    EXPECT_NEAR(LeastSquares(DataHandler_t_, 2)[1], 0.192, 0.001);
-    EXPECT_NEAR(LeastSquares(DataHandler_t_, 2)[2], -0.0144, 0.001);
+    EXPECT_NEAR(leastsquares.Calculate_value_at_point(2), 19, 0.001);
+    EXPECT_NEAR(leastsquares.Calculate_value_at_point(2.5), 37.875, 0.001);
+    EXPECT_NEAR(leastsquares.Calculate_value_at_point(4), 165, 0.001);
 }
 
 
